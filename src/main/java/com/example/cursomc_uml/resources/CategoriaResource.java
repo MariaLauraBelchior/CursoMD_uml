120ed4fc96762135bdba8d32ced36bbbe1a80f1c
MariaLauraBelchior <marialaurabelchior007@gmail.com>
2024-06-27 03:03:30 -0300

    Testando a primeira classe categoria

--- src/main/java/com/example/cursomc_uml/resources/CategoriaResource.java
+++ src/main/java/com/example/cursomc_uml/resources/CategoriaResource.java
@@ -1,6 +1,12 @@
 package com.example.cursomc_uml.resources;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.domain.Categoria;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -9,8 +15,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class CategoriaResource {
 
     @RequestMapping(method =RequestMethod.GET )
-    public String listar() {
-        return "Rest is working";
+    public List<Categoria> listar() {
+
+        Categoria cat1 = new Categoria(1, "Informatica");
+        Categoria cat2 = new Categoria(2, "Escritorio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
     
 }
